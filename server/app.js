7216d97477b20a832984e8b67e332348592fdcae
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-11-15 17:41:40 -0500

    chore: set cors in server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { routes } from "./routes/index.js";
 
 dotenv.config();
 const app = express();
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.urlencoded({ extended: true }));
 
 // routes
