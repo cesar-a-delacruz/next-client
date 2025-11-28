70056d185ba9da6e88fcb499cf3ce2d4bee89acf
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-11-27 20:15:12 -0500

    refactor: reduce code with Chart component

--- client/src/components/charts/CustomLineChart.jsx
+++ client/src/components/charts/CustomLineChart.jsx
@@ -1,50 +1,23 @@
-import { useEffect, useState } from 'react';
-import { LineChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Line } from 'recharts';
-import CustomTooltip from '@/components/charts/CustomTooltip';
+import Chart from '@/components/Chart';
 
-const COLORS = ['#FFBB28', '#00C49F', '#FF8042'];
+export default function CustomLineChart() {
+    const parser = (json) => {
+        const dateTimes = json.map((appointment) => {
+            let dateTime = new Date(appointment.dateTime.substring(0, 10));
+            dateTime.setDate(dateTime.getDate() + 1)
+            return { name: dateTime.toDateString(), value: 1 }
+        });
+        const appointments = dateTimes.reduce((acc, { name, value }) => {
+            acc[name] = (acc[name] || 0) + value;
+            return acc;
+        }, {});
 
-export default function CustomLineChart({ isAnimationActive = true }) {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        (async () => {
-            const result = await fetch(`http://localhost:3000/appointment/linechart/${"2025-11-08"}`);
-            const json = await result.json();
-            const appointments = json.map((appointment) => {
-                let dateTime = new Date(appointment.dateTime.substring(0, 10));
-                dateTime.setDate(dateTime.getDate() + 1)
-                return { name: dateTime.toDateString(), value: 1 }
-            });
-            const reduced = appointments.reduce((acc, { name, value }) => {
-                acc[name] = (acc[name] || 0) + value;
-                return acc;
-            }, {});
-
-            const ag = Object.keys(reduced).map((re) => {
-                return { name: re, value: reduced[re] }
-            })
-            console.log(ag)
-            setData([...ag])
-        })();
-    }, []);
+        const days = Object.keys(appointments).map((day) => {
+            return { name: day, value: appointments[day] }
+        })
+        return days;
+    }
     return (
-        <LineChart
-            style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
-            responsive
-            data={data}
-            margin={{
-                top: 50,
-                right: 30,
-                left: 20,
-                bottom: 5,
-            }}
-        >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip content={CustomTooltip} />
-            <Legend />
-            <Line type="monotone" name='appointments' dataKey="value" stroke="#8884d8" isAnimationActive={isAnimationActive} />
-        </LineChart>
+        <Chart type="linechart" date="2025-11-08" parser={parser} />
     );
 }
--- client/src/components/charts/CustomPieChart.jsx
+++ client/src/components/charts/CustomPieChart.jsx
@@ -1,60 +1,32 @@
-import { useEffect, useState } from 'react';
-import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
-import CustomTooltip from '@/components/charts/CustomTooltip';
-
-const COLORS = ['#FFBB28', '#00C49F', '#FF8042'];
+import Chart from '@/components/Chart';
 
 export default function CustomPieChart() {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        (async () => {
-            const result = await fetch(`http://localhost:3000/appointment/piechart/${"2025-11-08"}`);
-            const json = await result.json();
-            const appointments = {
-                completed: 0,
-                pending: 0,
-                cancelled: 0
-            };
-            json.forEach((item) => {
-                switch (item.status) {
-                    case "PENDING":
-                        appointments.pending++;
-                        break;
-                    case "COMPLETED":
-                        appointments.completed++;
-                        break;
-                    case "CANCELLED":
-                        appointments.cancelled++;
-                        break;
-                }
-            })
-            let vals = [];
-            for (const app in appointments) {
-                vals.push({ name: app, value: appointments[app] })
+    const parser = (json) => {
+        const appointments = {
+            completed: 0,
+            pending: 0,
+            cancelled: 0
+        };
+        json.forEach((item) => {
+            switch (item.status) {
+                case "PENDING":
+                    appointments.pending++;
+                    break;
+                case "COMPLETED":
+                    appointments.completed++;
+                    break;
+                case "CANCELLED":
+                    appointments.cancelled++;
+                    break;
             }
-            setData([...vals])
-        })();
-    }, []);
+        })
+        let statuses = [];
+        for (const status in appointments) {
+            statuses.push({ name: status, value: appointments[status] })
+        }
+        return statuses;
+    }
     return (
-        <PieChart width={400} height={400}>
-            <Pie
-                data={data}
-                cx={200}
-                cy={200}
-                labelLine={false}
-                outerRadius={150}
-                dataKey="value"
-                name='appointment statuses'
-            >
-                {
-                    data.map((entry, index) => (
-                        <Cell key={`cell-${index}`}
-                            fill={COLORS[index]}
-                        />))
-                }
-            </Pie>
-            <Legend />
-            <Tooltip content={CustomTooltip}></Tooltip>
-        </PieChart>
+        <Chart type="piechart" date="2025-11-08" parser={parser} />
     );
 }
