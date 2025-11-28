a43cb6955368d79072cf93fb1aa6228f25555625
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-11-28 11:48:33 -0500

    feat: add UserController with new method

---
+++ server/controllers/UserController.js
@@ -0,0 +1,22 @@
+import BaseController from "./BaseController.js";
+
+export default class UserController extends BaseController {
+    constructor(model, formParser) {
+        super(model, formParser);
+    }
+    findByPhone = async (req, res) => {
+        const { phone } = req.body;
+
+        try {
+            const row = await this.model.findUnique({
+                where: {
+                    phone
+                }
+            });
+            if (!row) return res.status(404).json({ error: "row not found" });
+            res.json(row);
+        } catch (error) {
+            res.status(500).json({ error: "Failed to find by Phone" });
+        }
+    };
+}
